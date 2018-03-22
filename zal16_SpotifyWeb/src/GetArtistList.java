e5e78772356046749626533a41777dc3e8de6587
Zachary <zal16@pitt.edu>
2018-03-21 20:09:01 -0400

    Lab 3 Web services

---
+++ zal16_SpotifyWeb/src/GetArtistList.java
@@ -0,0 +1,85 @@
+
+
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+/**
+ * Servlet implementation class GetArtistList
+ */
+@WebServlet("/GetArtistList")
+public class GetArtistList extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public GetArtistList() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		response.setContentType("application/json");  // You want to always specify how to return the data for web services. 
+		
+		String name = "";
+		
+		if(request.getParameter("band_name") != null){
+			name = request.getParameter("band_name");
+		}
+		
+		try {
+			DbUtilities db = new DbUtilities();
+			String sql = "SELECT * FROM artist ";
+			if(! name.equals("")){
+				sql += " WHERE band_name LIKE '%" + name + "%' "; 
+			}
+			sql +=" ORDER BY band_name ASC;";
+			
+			ResultSet rs = db.getResultSet(sql);
+			JSONArray artistList = new JSONArray();
+			while(rs.next()){
+				JSONObject artist = new JSONObject();
+				artist.put("id", rs.getString("artist_id"));
+				artist.put("first_name", rs.getString("first_name"));
+				artist.put("last_name", rs.getString("last_name"));
+				artist.put("band_name", rs.getString("band_name"));
+				artist.put("bio", rs.getInt("bio"));
+				artistList.put(artist);
+			}
+			
+			response.getWriter().write(artistList.toString());
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
